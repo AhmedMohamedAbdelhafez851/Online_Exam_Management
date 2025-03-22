78cd15e312c370bf84c97ba345e07f7c2d443a9e
AhmedMohamedAbdelhafez851 <102626727+AhmedMohamedAbdelhafez851@users.noreply.github.com>
2025-03-22 17:36:33 +0200

    Update Exam.cs

--- OnlineExamSystem.Domains/Entities/Exam.cs
+++ OnlineExamSystem.Domains/Entities/Exam.cs
@@ -1,18 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace OnlineExamSystem.Domains.Entities
 {
     public class Exam
     {
         public int ExamId { get; set; }
-        public string Title { get; set; } = ""; 
+
+        [Required(ErrorMessage = "Title is required")]  // ← THIS IS CRUCIAL
+        [StringLength(100, MinimumLength = 3)]
+        public string Title { get; set; } = "";
+
         public DateTime CreatedDate { get; set; }
         public string? CreatedBy { get; set; }
         public List<Question> Questions { get; set; } = new();
     }
 }
-
