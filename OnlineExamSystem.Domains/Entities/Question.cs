3bd007e806b75e03ace0c1b16552d582d6d341d3
AhmedMohamedAbdelhafez851 <102626727+AhmedMohamedAbdelhafez851@users.noreply.github.com>
2025-03-22 17:34:54 +0200

    Update Question.cs

--- OnlineExamSystem.Domains/Entities/Question.cs
+++ OnlineExamSystem.Domains/Entities/Question.cs
@@ -1,19 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace OnlineExamSystem.Domains.Entities
 {
     public class Question
     {
         public int QuestionId { get; set; }
-        public int ExamId { get; set; }
+
+        [Required(ErrorMessage = "Question text is required")]
+        [StringLength(500, MinimumLength = 3, ErrorMessage = "Question must be between {2}-{1} characters")]
         public string Title { get; set; } = "";
-        public int? CorrectChoiceId { get; set; } // Foreign key to the correct Choice
-        public Choice CorrectChoice { get; set; } // Navigation property for the correct Choice
-        public Exam Exam { get; set; } = new();
+
+        public int ExamId { get; set; }
+        public int? CorrectChoiceId { get; set; }
+        public Choice CorrectChoice { get; set; }
+        public Exam Exam { get; set; } 
         public List<Choice> Choices { get; set; } = new();
     }
 }
